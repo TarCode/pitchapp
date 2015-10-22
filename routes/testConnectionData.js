ddbed279748f6d55ec1d4e778cde62d7817afe0b
TarCode <taariq@projectcodex.co>
2015-10-22 09:53:24 +0200

    edited travis config files

--- routes/testConnectionData.js
+++ routes/testConnectionData.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 module.exports = function () {
   return mysql.createConnection({
-    adapter: "mysql",
+    host: " 127.0.0.1",
     database: "pitchapp_test",
     username: "travis",
     encoding: "utf8"
