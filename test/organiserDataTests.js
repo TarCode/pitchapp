78da856027a72c8caa0ea7b942f4aba921ea3972
TarCode <taariq@projectcodex.co>
2015-10-22 10:24:20 +0200

    edited travis config files once more

--- test/organiserDataTests.js
+++ test/organiserDataTests.js
@@ -1,5 +1,6 @@
 var OrganiserDataService = require('../dataServices/organiserDataServicePromise');
 var assert = require("assert");
+var mysql = require('mysql');
 var Promise = require("bluebird");
 var connection = mysql.createConnection({
   host     : 'localhost',
